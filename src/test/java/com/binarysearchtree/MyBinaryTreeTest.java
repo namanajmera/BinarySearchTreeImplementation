91ee41d93ece178dbcc45af3f0e133782168d41b
Naman <cool.naman.ajmera@gmail.com>
2021-05-07 11:32:21 +0530

    Ability to complete the BT

--- src/test/java/com/binarysearchtree/MyBinaryTreeTest.java
+++ src/test/java/com/binarysearchtree/MyBinaryTreeTest.java
@@ -13,4 +13,24 @@ public class MyBinaryTreeTest {
 
         Assertions.assertEquals(3, myBinaryTree.size());
     }
+
+    @Test
+    public void givenAllNumbers_WhenAddedToBinaryTree_ShouldReturnSizeAsTotalCountOfNumbers() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        myBinaryTree.add(22);
+        myBinaryTree.add(40);
+        myBinaryTree.add(60);
+        myBinaryTree.add(95);
+        myBinaryTree.add(11);
+        myBinaryTree.add(3);
+        myBinaryTree.add(16);
+        myBinaryTree.add(65);
+        myBinaryTree.add(63);
+        myBinaryTree.add(67);
+
+        Assertions.assertEquals(13, myBinaryTree.size());
+    }
 }
